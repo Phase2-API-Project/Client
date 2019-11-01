2b876b2d71e2d7606536b24d943a22ae39579158
laskarks <laskar.ksatria12@gmail.com>
2019-11-01 15:41:33 +0700

    Final-API

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 $('document').ready(function () {
 
     if (localStorage.getItem('token')) {
@@ -6,7 +8,9 @@ $('document').ready(function () {
     $('#REGISTER').hide()
     $('#NAVBAR').hide()
     $('#logout-button').hide()
-    $('#MAINPAGE').hide()
+    $('#MAIN').hide()
+    $('#FINDDOCTOR').hide()
+    $('#DOCTORS').hide()
     }
 
     $('#register-page').click(registerShow)
@@ -14,7 +18,8 @@ $('document').ready(function () {
     $('#button-login').click(login)
     $('#logout-button').click(logout)
     $('#button-register').click(register)
-    
+    $('#finddoctor-btn').click(findDoctor)
+    $('#search-doctor').click(findDoctorForm)
 })
 
 function registerShow () {
@@ -32,7 +37,7 @@ $('#REGISTER').slideDown(800, function () {
 })
 }
 
-function login () {panda
+function login () {
 
 event.preventDefault()
 const email = $('#email-login').val()
@@ -44,11 +49,13 @@ $.ajax({
         data: { email, password }
     })
     .done(response => {
+        $('#FINDDOCTOR').hide()
+        $('#DOCTORS').hide()
         $('#LOGIN').slideToggle(1000, function () {
             $('#NAVBAR').show(800, function () {
                 $('#logout-button').show(1000,function () {
                      localStorage.setItem('token', response.token)
-                    $('#MAINPAGE').show('slow')
+                    $('#MAIN').show('slow')
                 })
             })
 
@@ -57,9 +64,14 @@ $.ajax({
         $('#email-login').val('')
         $('#password-login').val('')
     })
-    .fail((jqXHR, textstatus) => {
-        // swal("Good job!", "Invalid Email / Password", "error");
-        alert(textstatus)
+    .fail((err) => {
+        let error = JSON.stringify(err)
+        Swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: error.responseText,
+            footer: '<a href>Why do I have this issue?</a>'
+          })
     })
 }
 
@@ -68,23 +80,29 @@ function isLogin () {
 $('#LOGIN').hide()
 $('#REGISTER').hide()
 $('#NAVBAR').show()
-$('#MAINPAGE').show()
+$('#MAIN').show()
 $('#logout-button').show()
+
 }
 
 function isLogout () {
-$('#MAINPAGE').hide()
+$('#MAIN').hide()
 $('#NAVBAR').hide()
 $('#LOGIN').show()
 $('#REGISTER').hide()
+$('#FINDDOCTOR').hide()
+$('#DOCTORS').hide()
 }
 
 function logout() {
 $('#logout-button').hide('slow', function () {
     localStorage.removeItem('token')
     $('#NAVBAR').hide('slow',function () {
-        $('#MAINPAGE').hide('slow', function () {
+        $('#MAIN').hide('slow', function () {
             $('#REGISTER').hide()
+            $('#MAIN').hide()
+            $('#FINDDOCTOR').hide()
+            $('#DOCTORS').hide()
             $('#LOGIN').show('slow')
         })
     })
@@ -117,7 +135,176 @@ $.ajax({
     loginShow()
 })
 .fail(function (err) {
-    console.log(err)
+    let error = JSON.stringify(err)
+    Swal.fire({
+        type: 'error',
+        title: 'Oops...',
+        text: error.responseText,
+        footer: '<a href>Why do I have this issue?</a>'
+      })
 })
 
 }
+
+function findDoctor () {
+    $('#MAIN').slideToggle('slow', function () {
+        $('#MAIN').hide('slow', function () {
+            $("#FINDDOCTOR").show('slow')
+        })
+    })
+}
+
+function findDoctorForm () {
+    event.preventDefault()
+    console.log('masuk form')
+    let location = $('#add-location').val()
+    console.log("-"+location+"-")
+    console.log(localStorage.getItem('token'))
+    $.ajax({
+        url: 'http://localhost:3000/loc/get',
+        method: 'POST',
+        data: {
+            place: location
+        },
+        headers: {
+            token : localStorage.getItem('token')
+        }
+    })
+    .done ( (data) => {
+        
+        getDoctor(data)
+    })
+    .fail (function(err) {
+       
+        let error = JSON.stringify(err)
+        Swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: error.responseText,
+            footer: '<a href>Why do I have this issue?</a>'
+          })
+    })
+}   
+
+
+function getDoctor (location) {
+    
+    let speciality = $('#add-spesiality').val()
+    console.log(speciality)
+    let gender = $('#add-gender').val()
+    console.log(gender)
+    console.log(location.lat);
+    console.log(location.lng);
+
+    
+    $.ajax({
+        url: `http://localhost:3000/doctor/get`,
+        method: 'POST',
+        data: {
+            lat: location.lat,
+            long: location.lng,
+            speciality_uid: speciality,
+            gender: gender
+        },
+        headers: {
+            token : localStorage.getItem('token')
+        }
+    })
+    .done(function (data) {
+        getDoctorList(data)
+    })
+    .fail(function (err) {
+        let error = JSON.stringify(err)
+        Swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: error.responseText,
+            footer: '<a href>Why do I have this issue?</a>'
+          })
+    })
+}
+
+
+function getDoctorList(doctors) {
+    console.log(doctors)
+    $('#FINDDOCTOR').hide('slow')
+    $('#DOCTORS').show('slow')
+
+    let avatar = ["elliot", "helen", "jenny", "veronika", "stevie", "steve", "elliot", "helen", "jenny", "veronika", "stevie", "steve"]
+    
+    doctors.forEach((doctor, index) => {
+        console.log(doctor)
+        let talent = doctor.specialties.map(el => {
+            return el.name
+        })
+        talent.join(' ,')
+        $("#doctors-list").append(`
+            <h3>biar keliatan</h3>
+            <div class="ui card" style=" width:200px">
+                <div class="image">
+                    <img src="https://semantic-ui.com/images/avatar/large/${avatar[index]}.jpg">
+                </div>
+                <div class="content" style=" height:100px">
+                    <a class="header" style=" width:200px">${doctor.name}</a>
+                    <div class="meta">
+                        <span class="phone">${doctor.gender}</span>
+                    </div>
+                    <div class="description" name="bio">
+                        ${doctor.bio}
+                    </div>
+                </div>
+                <div class="extra content">
+                    ${talent}
+                </div>
+            </div>
+            `)
+    })
+}
+
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    const config = {
+        host: 'http://localhost:3000'
+    }
+    const googleToken = googleUser.getAuthResponse().id_token;
+
+    $.ajax({
+            method: 'POST',
+            url: `${config.host}/users/googlelogin`,
+            data: {
+                token: googleToken
+            }
+        })
+        .done(response => {
+        $('#FINDDOCTOR').hide()
+        $('#DOCTORS').hide()
+        $('#LOGIN').slideToggle(1000, function () {
+            $('#NAVBAR').show(800, function () {
+                $('#logout-button').show(1000,function () {
+                     localStorage.setItem('token', response.token)
+                    $('#MAIN').show('slow')
+                })
+            })
+
+        })
+        $('#username-login').val('')
+        $('#email-login').val('')
+        $('#password-login').val('')
+        })
+        .fail(err => {
+            let error = JSON.stringify(err)
+            Swal.fire({
+                type: 'error',
+                title: 'Oops...',
+                text: error.responseText,
+                footer: '<a href>Why do I have this issue?</a>'
+              })
+        })
+
+} //
